2aea1ef8c81a8a1c5bd07fe86feb542dfd9455de
yxh <723732636@qq.com>
2019-05-12 15:39:09 +0800

    save13

---
+++ src/test/java/Num13Test.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Num13Test {
+
+    String s1 = "IV";
+    String s2 = "III";
+    String s3 = "IX";
+    String s4 = "LVIII";
+    String s5 = "MCMXCIV";
+
+    Num13 no13 = new Num13();
+    @Test
+    public void romanToInt() {
+        assertEquals(4, no13.romanToInt(s1));
+        assertEquals(3, no13.romanToInt(s2));
+        assertEquals(9, no13.romanToInt(s3));
+        assertEquals(58, no13.romanToInt(s4));
+        assertEquals(1994, no13.romanToInt(s5));
+
+    }
+}
