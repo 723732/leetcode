277cdbea95f2d3604c040689f5199138017c4f8a
yxh <723732636@qq.com>
2019-07-18 22:12:14 +0800

    save

---
+++ src/main/java/Tree/BuildTree.java
@@ -0,0 +1,8 @@
+package Tree;
+
+public class BuildTree {
+    //从中序与后序遍历构造二叉树
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+
+    }
+}
--- src/main/java/Tree/TreehasPathSum.java
+++ src/main/java/Tree/TreehasPathSum.java
@@ -5,18 +5,17 @@ import java.util.Stack;
 public class TreehasPathSum {
     //可以递减
     public boolean hasPathSum(TreeNode root, int sum) {
+        if(root == null)
+            return false;
 
-    }
+        if (root != null)
+            sum = sum - root.val;
 
-    //递归好难，到底咋写
-    public int pathSum(TreeNode node){
-        int sum = 0;
-        int left_sum = 0;
-        int right_sum = 0;
+        if (root.left == null && root.right == null && sum == 0)
+            return true;
 
-        if (node != null){
-            left_sum = sum + pathSum(node.left);
-            right_sum = sum + pathSum(node.right);
-        }
+        return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+
+
     }
 }
